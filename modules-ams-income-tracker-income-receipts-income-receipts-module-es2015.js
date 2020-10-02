f29b36f96d3e9848f0f5a555cd2036ba55ca2101
senthilkumarcode <senthilkumar.snp@gmail.com>
2020-10-02 16:29:26 +0530

    Auto-generated commit

--- modules-ams-income-tracker-income-receipts-income-receipts-module-es2015.js
+++ modules-ams-income-tracker-income-receipts-income-receipts-module-es2015.js
@@ -541,7 +541,7 @@ let IncomeViewReceiptsComponent = class IncomeViewReceiptsComponent {
                     let elemId = 'receiptReverse' + row;
                     return '<div class="simple-actions">'
                         + '<a href="javascript:void(0)" class=" ' + isReverse(!value) + ' " id="' + elemId + '" onClick="editReverseEvent(' + row + ')">'
-                        + '<i class="fa fa-undo text-red-900" aria-hidden="true"></i>'
+                        + '<svg class="reverse" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-rotate-ccw"><polyline points="1 4 1 10 7 10"></polyline><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"></path></svg>'
                         + '</a>'
                         + '<a href="javascript:void(0)" class="squares text-purple-100 bg-purple-500 no-link ' + isReverse(value) + '">R</a>'
                         + '</div>';
